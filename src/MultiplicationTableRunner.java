313f4d090a0520cd1b7b0c5dfc504f45e4e1c8e8
Yasir <yasir391245@gmail.com>
2024-11-25 16:22:29 +0600

    NumberOperation

--- src/MultiplicationTableRunner.java
+++
@@ -1,6 +0,0 @@
-public class MultiplicationTableRunner {
-    public static void main(String[] args) {
-        MultiplicationTable table = new MultiplicationTable();
-        table.printTable();
-    }
-}
