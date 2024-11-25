359b88d2e2509dc6d607664ee936f4e03c64d9bc
Yasir <yasir391245@gmail.com>
2024-11-25 16:27:42 +0600

    PowerCalculatorRunner

---
+++ src/PowerCalculatorRunner.java
@@ -0,0 +1,12 @@
+public class PowerCalculatorRunner {
+
+    public static void main(String[] args) {
+
+        PowerCalculator powerCalculator = new PowerCalculator();
+
+        System.out.println("\n\tPrinting results:");
+
+        powerCalculator.powerOfTwo();
+
+    }
+}
